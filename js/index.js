1b11c1b313b3567d7b41ebfd6a6c939f93a53be5
mohamed zakaria <mohamed_zakaria@gmail.com>
2025-11-29 20:36:00 +0200

    Add main

--- js/index.js
+++
@@ -1,8 +0,0 @@
-// israa
-let signUpBtn = document.querySelector('.sign-up-btn-js');
-
-document.addEventListener('click', () => {
-    console.log('sign Up Btn clicked');
-});
-
-// end of israa's part
